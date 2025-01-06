693fe4901a529efb818dd03a13572b056479eed0
Kaan Kara <kaankara1987@gmail.com>
2025-01-07 01:03:25 +0300

    Initial commit

---
+++ src/main/java/org/xhite/webuy/WebuyApplication.java
@@ -0,0 +1,13 @@
+package org.xhite.webuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebuyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xhite/webuy/WebuyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xhite.webuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebuyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
